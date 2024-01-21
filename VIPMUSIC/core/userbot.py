83c00570b7e4b1062d49b71737679df2031158af
Vivekkumar07089 <155367948+Vivekkumar07089@users.noreply.github.com>
2024-01-21 12:36:18 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -8,7 +8,10 @@ load_dotenv()
 import config
 from dotenv import load_dotenv
 from ..logging import LOGGER
-
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+STRING_SESSION = getenv("STRING_SESSION", "")
+TEST_ID = int("-1002042572827")
 
 assistants = []
 assistantids = []
