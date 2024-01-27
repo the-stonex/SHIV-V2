59072708755aad6adc499bd85bd6086a63b9bd97
Vivekkumar07089 <155367948+Vivekkumar07089@users.noreply.github.com>
2024-01-27 08:09:18 +0530

    Update song.py

--- VIPMUSIC/plugins/tools/song.py
+++ VIPMUSIC/plugins/tools/song.py
@@ -6,7 +6,7 @@ from pyrogram import filters
 from youtube_search import YoutubeSearch
 from ... import app
 
-from .... import SUPPORT_CHAT,BOT_NAME
+from Config import SUPPORT_CHAT,BOT_NAME
 
 
 def time_to_seconds(time):
