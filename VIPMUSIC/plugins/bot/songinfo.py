6f46f4fbb4a2900a6560bd80357b6d639df6ed3f
Vivekkumar07089 <155367948+Vivekkumar07089@users.noreply.github.com>
2024-02-24 08:03:58 +0530

    Update and rename eti.py to songinfo.py

---
+++ VIPMUSIC/plugins/bot/songinfo.py
@@ -0,0 +1,95 @@
+import time
+import random
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from youtubesearchpython.__future__ import VideosSearch
+
+import config
+from VIPMUSIC import app
+from VIPMUSIC.misc import _boot_
+from VIPMUSIC.plugins.sudo.sudoers import sudoers_list
+from VIPMUSIC.utils.database import (
+    add_served_chat,
+    add_served_user,
+    blacklisted_chats,
+    get_lang,
+    is_banned_user,
+    is_on_off,
+)
+from VIPMUSIC.utils.decorators.language import LanguageStart
+from VIPMUSIC.utils.formatters import get_readable_time
+from VIPMUSIC.utils.inline import help_pannel, private_panel, start_panel
+from config import BANNED_USERS
+from strings import get_string
+
+
+
+YUMI_PICS = [
+"https://telegra.ph/file/5d33ec7c387985d78352b.jpg",
+"https://telegra.ph/file/3095010ce25b4c55f9823.jpg",
+"https://telegra.ph/file/62de4fe21cc8575298dd9.jpg",
+"https://telegra.ph/file/62de4fe21cc8575298dd9.jpg",
+"https://telegra.ph/file/6f93dcaa2c5943253c149.jpg",
+"https://telegra.ph/file/cbbfe284b42c403a6bbbd.jpg",
+"https://telegra.ph/file/6f93dcaa2c5943253c149.jpg",
+"https://telegra.ph/file/2e909e7dd709b380918f7.jpg",
+"https://telegra.ph/file/7f0490e53d3cce83504af.jpg",
+"https://telegra.ph/file/842c0a55f982d30fe4f48.jpg",
+"https://telegra.ph/file/585cfc47e2240f2ca3dce.jpg",
+"https://telegra.ph/file/2329f335339f63b2bbd5c.jpg"
+
+]
+
+
+
+@app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
+@LanguageStart
+async def start_pm(client, message: Message, _):
+    await add_served_user(message.from_user.id)
+    if len(message.text.split()) > 1:
+        name = message.text.split(None, 1)[1]
+        
+        if name[0:3] == "inf":
+            m = await message.reply_text("🔎")
+            query = (str(name)).replace("info_", "", 1)
+            query = f"https://www.youtube.com/watch?v={query}"
+            results = VideosSearch(query, limit=1)
+            for result in (await results.next())["result"]:
+                title = result["title"]
+                duration = result["duration"]
+                views = result["viewCount"]["short"]
+                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+                channellink = result["channel"]["link"]
+                channel = result["channel"]["name"]
+                link = result["link"]
+                published = result["publishedTime"]
+            searched_text = _["start_6"].format(
+                title, duration, views, published, channellink, channel, app.mention
+            )
+            key = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text=_["S_B_8"], url=link),
+                        InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
+                    ],
+                ]
+            )
+            await m.delete()
+            await app.send_photo(
+                chat_id=message.chat.id,
+                photo=thumbnail,
+                caption=searched_text,
+                reply_markup=key,
+            )
+            if await is_on_off(2):
+                return await app.send_message(
+                    chat_id=config.LOGGER_ID,
+                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                )
+              await app.send_message(
+                    chat_id=config.LOGGER_ID,
+                    text=f"{message.from_user.mention}",
+            )
+          
+        
