9f31c9396450a65d27c045335b3190bcb6b89718
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-20 19:25:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,54 +5,61 @@ from dotenv import load_dotenv
 from pyrogram import filters
 
 load_dotenv()
-
+#________________________________________________________________________________#
 # Get this value from my.telegram.org/apps
+
 API_ID = int(getenv("API_ID"))
+#________________________________________________________________________________#
 API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
+#________________________________________________________________________________#
 BOT_TOKEN = getenv("BOT_TOKEN")
+#________________________________________________________________________________#
 BOT_NAME  = getenv("BOT_NAME")
+#________________________________________________________________________________#
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-
+#________________________________________________________________________________#
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
-
+#________________________________________________________________________________#
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID"))
-
+#________________________________________________________________________________#
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID"))
-
+#________________________________________________________________________________#
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-
+#________________________________________________________________________________#
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/Vivekkumar0708/Music-plugin-for-mukeshbot",
+    "UPSTREAM_REPO",   "https://github.com/Vivekkumar0708/Music-plugin-for-mukeshbot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
-
+#________________________________________________________________________________#
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Quizess_prince")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Quizess_prince")
-
+#________________________________________________________________________________#
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
-
+#________________________________________________________________________________#
 # Get this credentials from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "19609edb1b9f4ed7be0c8c1342039362")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "409e31d3ddd64af08cfcc3b0f064fcbe")
 
 
+#________________________________________________________________________________#
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 2500))
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", 100))
+
+#________________________________________________________________________________#
 
 # Telegram audio and video file size limit (in bytes)
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
@@ -68,13 +75,12 @@ STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
 
 
-#    __      _______ _____    ___  __ _    _  _____ _____ _____   _____   ____ _______ 
-#    \ \    / /_   _|  __ \   |  \/  | |  | |/ ____|_   _/ ____|  |  _ \ / __ \__   __|
-#     \ \  / /  | | | |__) |  | \  / | |  | | (___   | || |       | |_) | |  | | | |   
-#      \ \/ /   | | |  ___/   | |\/| | |  | |\___ \  | || |       |  _ <| |  | | | |   
-#       \  /   _| |_| |       | |  | | |__| |____) |_| || |____   | |_) | |__| | | |   
-#        \/   |_____|_|       |_|  |_|\____/|_____/|_____\_____|  |____/ \____/  |_|   
-
+#     _    __      _______ _____    ___  __ _    _  _____ _____ _____   _____   ____ _______ 
+#    | |  / /     |__   __|  __ \  |__ \/_ | |  | |/ ____|_   _/ ____| |  __ \ / __ \__   __|
+#    | | / /         | |  | |__) |    ) || | |  | | (___   | || |      | |__) | |  | | | |   
+#    | |/ /          | |  |  ___/    / / | | |  | |\___ \  | || |      |  _  /| |  | | | |   
+#    | |\ \          | |  | |       / /_ | | |__| |____) |_| || |____  | | \ \| |__| | | |   
+#    |_| \_\         |_|  |_|      |____||_|\____/|_____/|_____\_____| |_|  \_\\____/  |_|
 
 
 
@@ -84,24 +90,74 @@ lyrical = {}
 votemode = {}
 autoclean = []
 confirmer = {}
+#________________________________________________________________________________#
 
+#images
 
 START_IMG_URL = getenv(
     "START_IMG_URL", "https://telegra.ph/file/2e909e7dd709b380918f7.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/7f0490e53d3cce83504af.jpg"
+    "PING_IMG_URL",
+    "https://telegra.ph/file/91533956c91d0fd7c9f20.jpg",
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/3095010ce25b4c55f9823.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/5d33ec7c387985d78352b.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/2e909e7dd709b380918f7.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/dd799cb0652f1462e31f1.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/7f0490e53d3cce83504af.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/842c0a55f982d30fe4f48.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/ebe5acccb2e9ddf15d714.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/585cfc47e2240f2ca3dce.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/2329f335339f63b2bbd5c.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/2329f335339f63b2bbd5c.jpg"
+
+PLAYLIST_IMG_URL = getenv(
+    "PLAYLIST_IMG_URL",
+    "https://telegra.ph/file/f4edfbd83ec3150284aae.jpg",
+)
+
+GLOBAL_IMG_URL = getenv(
+    "GLOBAL_IMG_URL",
+    "https://telegra.ph/file/de1db74efac1770b1e8e9.jpg",
+)
+
+STATS_IMG_URL = getenv(
+    "STATS_IMG_URL",
+    "https://telegra.ph/file/4dd9e2c231eaf7c290404.jpg",
+)
+
+TELEGRAM_AUDIO_URL = getenv(
+    "TELEGRAM_AUDIO_URL",
+    "https://telegra.ph/file/8234d704952738ebcda7f.jpg",
+)
+
+TELEGRAM_VIDEO_URL = getenv(
+    "TELEGRAM_VIDEO_URL",
+    "https://telegra.ph/file/8d02ff3bde400e465219a.jpg",
+)
+
+STREAM_IMG_URL = getenv(
+    "STREAM_IMG_URL",
+    "https://telegra.ph/file/e24f4a5f695ec5576a8f3.jpg",
+)
+
+SOUNCLOUD_IMG_URL = getenv(
+    "SOUNCLOUD_IMG_URL",
+    "https://telegra.ph/file/7645d1e04021323c21db9.jpg",
+)
+
+YOUTUBE_IMG_URL = getenv(
+    "YOUTUBE_IMG_URL",
+    "https://telegra.ph/file/76d29aa31c40a7f026d7e.jpg",
+)
+
+SPOTIFY_ARTIST_IMG_URL = getenv(
+    "SPOTIFY_ARTIST_IMG_URL",
+    "https://telegra.ph/file/b7758d4e1bc32aa9fb6ec.jpg",
+)
+
+SPOTIFY_ALBUM_IMG_URL = getenv(
+    "SPOTIFY_ALBUM_IMG_URL",
+    "https://telegra.ph/file/60ed85638e00df10985db.jpg",
+)
+
+SPOTIFY_PLAYLIST_IMG_URL = getenv(
+    "SPOTIFY_PLAYLIST_IMG_URL",
+    "https://telegra.ph/file/f4edfbd83ec3150284aae.jpg",
+)
+
+#________________________________________________________________________________#
 
 
 def time_to_seconds(time):
