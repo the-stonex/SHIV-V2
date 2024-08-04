9f50243dac21cdb379b104d894ee2c51bcb545e8
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-04 16:53:25 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -1,16 +1,6 @@
 from pyrogram import Client
-from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
 import config
-from dotenv import load_dotenv
 from ..logging import LOGGER
-
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "")
-STRING_SESSION = getenv("STRING_SESSION", "")
-TEST_ID = int("-1002042572827")
 
 assistants = []
 assistantids = []
@@ -59,11 +49,6 @@ class Userbot(Client):
 
         if config.STRING1:
             await self.one.start()
-            try:
-                await self.one.join_chat("THE_VIP_BOY")
-
-            except:
-                pass
             assistants.append(1)
             try:
                 await self.one.send_message(config.LOGGER_ID, "Assistant Started !")
@@ -81,11 +66,6 @@ class Userbot(Client):
 
         if config.STRING2:
             await self.two.start()
-            try:
-
-                await self.two.join_chat("VIP_CREATORS")
-            except:
-                pass
             assistants.append(2)
             try:
                 await self.two.send_message(config.LOGGER_ID, "Assistant Started")
@@ -102,11 +82,6 @@ class Userbot(Client):
 
         if config.STRING3:
             await self.three.start()
-            try:
-
-                await self.three.join_chat("VIP_CREATORS")
-            except:
-                pass
             assistants.append(3)
             try:
                 await self.three.send_message(config.LOGGER_ID, "Assistant Started")
@@ -123,11 +98,6 @@ class Userbot(Client):
 
         if config.STRING4:
             await self.four.start()
-            try:
-
-                await self.four.join_chat("VIP_CREATORS")
-            except:
-                pass
             assistants.append(4)
             try:
                 await self.four.send_message(config.LOGGER_ID, "Assistant Started")
@@ -144,11 +114,6 @@ class Userbot(Client):
 
         if config.STRING5:
             await self.five.start()
-            try:
-
-                await self.five.join_chat("VIP_CREATORS")
-            except:
-                pass
             assistants.append(5)
             try:
                 await self.five.send_message(config.LOGGER_ID, "Assistant 5 started !")
