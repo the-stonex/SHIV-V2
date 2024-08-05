c6e3f3b2b7576ae37a41c5bdcdceb87f6cd58a4f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-05 12:54:58 +0530

    Update database.py

--- VIPMUSIC/utils/database.py
+++ VIPMUSIC/utils/database.py
@@ -525,7 +525,10 @@ async def get_served_chats() -> list:
     async for chat in chatsdb.find({"chat_id": {"$lt": 0}}):
         chats_list.append(chat)
     return chats_list
-
+    
+async def delete_served_chat(chat_id: int):
+    await chatsdb.delete_one({"chat_id": chat_id})
+        
 
 async def is_served_chat(chat_id: int) -> bool:
     chat = await chatsdb.find_one({"chat_id": chat_id})
