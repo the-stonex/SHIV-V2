9a0c27921c527b0747c6daf5a4b275a1ec6e61c6
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-06 18:50:49 +0530

    Update database.py

--- VIPMUSIC/utils/database.py
+++ VIPMUSIC/utils/database.py
@@ -46,13 +46,18 @@ playlist = []
 
 #connect 
 async def connect_to_chat(user_id: int, chat_id: int):
+    existing = await connectdb.find_one({'user_id': user_id, 'chat_id': chat_id})
+    if existing:
+        return True
+
     result = await connectdb.update_one(
         {'user_id': user_id},
         {'$set': {'chat_id': chat_id}},
         upsert=True
     )
+    
     return result.modified_count > 0 or result.upserted_id is not None
-
+ 
 async def get_connected_chat(user_id: int):
     user = await connectdb.find_one({'user_id': user_id}, {'_id': 0, 'chat_id': 1})
     return user['chat_id'] if user and 'chat_id' in user else False
