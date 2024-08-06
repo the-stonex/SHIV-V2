1fe56f0450fba2be678d0b9c1bb5fc45bd801427
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-06 18:15:52 +0530

    Update connection.py

--- VIPMUSIC/plugins/admins/connection.py
+++ VIPMUSIC/plugins/admins/connection.py
@@ -12,7 +12,9 @@ async def auth(client, message: Message):
     admin_ids = [ member.user.id async for member in app.get_chat_members(message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS)]
     if not message.from_user.id in admin_ids:
         return 
+    user_id = message.from_user.id
+    chat_id = message.chat.id
    # re = await connect_to_chat(message.from_user.id, message.chat.id)
     
-    markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴄᴏɴɴᴇᴄᴛ ᴛᴏ ᴄʜᴀᴛ ", url=f"http://t.me/{app.username}?start=connect_{message.chat.id}")]])
+    markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴄᴏɴɴᴇᴄᴛ ᴛᴏ ᴄʜᴀᴛ ", url=f"http://t.me/{app.username}?start=connect_{user_id}_{chat_id}")]])
     await message.reply_text("ᴛᴀᴘ ᴛʜᴇ ғᴏʟʟᴏᴡɪɴɢ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ᴛᴏ ᴛʜɪs ᴄʜᴀᴛ ɪɴ ᴘᴍ", reply_markup = markup)
